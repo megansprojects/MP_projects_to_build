232b0b8194c725dd5f62af7bede24ce160a7d36a
Megan's Projects <72683581+megansprojects@users.noreply.github.com>
2023-09-09 07:55:22 +0100

    BG3 picture
    BG3 picture

---
+++ BG3Picture/arduino/BG3picture/BG3picture.ino
@@ -0,0 +1,20 @@
+
+//                    _
+// |\/| _  _  _  _  _|_) __  o _  __| _
+// |  |(/_(_|(_|| |_>|  | (_)|(/_(_ |_>
+//         _|               _|
+//       www.megansprojects.com
+// 
+// Baulders gate 3 laser cut picture
+
+#define LED_PIN 10
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+}
